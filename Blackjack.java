f5eb89b232610b1340f981a55e9c53d2373168f1
noahterbest <noahterbest@gmail.com>
2024-02-21 22:52:43 -0700

    Added game loop and notated project.

--- Blackjack.java
+++ Blackjack.java
@@ -1,5 +1,21 @@
+// Noah TerBest - Blackjack
+// GitHub: https://github.com/noahterbest/Blackjack
+
 public class Blackjack {
     public static void main(String[] args) {
+        // Introduction
         System.out.println("Hello! This is a blackjack game");
+
+        // Variables
+        int dealerHand = 0;
+        int playerHand = 0;
+
+        // Game loop (plays game 5 times)
+        for (int i = 0; i < 5; i++) {
+            // Game code goes here
+
+            System.out.println("Hello world!");
+
+        }
     }
 }
