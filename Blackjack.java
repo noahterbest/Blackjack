9c14c28be647cb4d4a58cd7c6ca70c720e428bc3
noahterbest <noahterbest@gmail.com>
2024-02-22 22:19:38 -0700

    Reworked game loop, and added a few basic mechanics.

--- Blackjack.java
+++ Blackjack.java
@@ -1,21 +1,43 @@
 // Noah TerBest - Blackjack
 // GitHub: https://github.com/noahterbest/Blackjack
 
+import java.util.Scanner;
+import java.util.Random;
+
 public class Blackjack {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         // Introduction
         System.out.println("Hello! This is a blackjack game");
+        System.out.println("--------------------------------");
 
         // Variables
-        int dealerHand = 0;
-        int playerHand = 0;
+        int playerCash = 100;
+        int playerBet = 0;
+        int dealerCard1 = 0;
+        int dealerCard2 = 0;
+        int playerCard1 = 0;
+        int playerCard2 = 0;
 
-        // Game loop (plays game 5 times)
-        for (int i = 0; i < 5; i++) {
-            // Game code goes here
+        // Game loop
+        while (playerCash > 0) {
+            System.out.println("You have $" + playerCash);
+            System.out.println("How much would you like to bet?: ");
 
-            System.out.println("Hello world!");
+            // Get bet
+            playerBet = scanner.nextInt();
+            playerCash -= playerBet;
+            System.out.println("You bet $" + playerBet);
+            System.out.println("--------------------------------");
 
+            // Deal cards
+            dealerCard1 = (int) (Math.random() * 11) + 1;
+            dealerCard2 = (int) (Math.random() * 11) + 1;
+            playerCard1 = (int) (Math.random() * 11) + 1;
+            playerCard2 = (int) (Math.random() * 11) + 1;
         }
+        System.out.println("The dealer shows " + dealerCard1);
+        System.out.println("You have " + playerCard1 + " and " + playerCard2);
     }
 }
